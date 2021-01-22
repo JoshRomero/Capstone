d5d494010d0861b7924c0794784fd53f7be8fd3b
William Francis <wmf006@latech.edu>
2021-01-22 03:53:46 -0600

    Basic GUI Setup in Tkinter with Text Demo

--- Code/Python GUI.py
+++ Code/Python GUI.py
@@ -1,3 +1,27 @@
-import tkinter as tk
+# Code from Geek for Geeks
 
-window = tk.Tk()
+from tkinter import *
+
+root = Tk()
+root.title("Object Finder Application")
+root.geometry('500x300')
+
+
+select = Label(root, text = "What would you like to search for?")
+select.pack()
+
+txt = Entry(root, width=10)
+txt.pack() 
+
+
+def clicked():
+    res = "Searching for: " + txt.get()
+    select.configure(text = res)
+ 
+btn = Button(root, text = "Search" ,
+             fg = "red", command=clicked)
+btn.pack()
+
+
+
+root.mainloop()
