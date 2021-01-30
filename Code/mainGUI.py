d19a5f379ea7b76b09c5f19bf5a0a2324d333ea6
NathanHegab <37705678+NathanHegab@users.noreply.github.com>
2021-01-30 16:55:42 -0600

    Combined

--- Code/mainGUI.py
+++ Code/mainGUI.py
@@ -26,68 +26,19 @@ mainText1 = Label(main, text = " ")
 mainText1.grid(column=2, row=7)
 
 
-# Initialize the recognizer  
-r = sr.Recognizer()  
-  
-# Function to convert text to 
-# speech 
-def SpeakText(command): 
-      
-    # Initialize the engine 
-    engine = pyttsx3.init() 
-    engine.say(command)  
-    engine.runAndWait()
-
 def textFunctionCall():
-    # checks if textbox is empty
-    if textInput1.get() == "" :
-        temporary = "Error: Text Field is Empty"
-        mainText1.configure(text = temporary)
-    else :
-        # calls text function
-        text.textFunction(textInput1.get())
-
-        # updates mainText1
-        temporary = "Text Sent to Server"
-        mainText1.configure(text = temporary)
-
+    # calls text function
+    temporary = text.textFunction(textInput1.get())
+    mainText1.configure(text = temporary)
 
 def microphoneFunctionCall():
-    flag = True
-    while(flag):     
-        
-        # Exception handling to handle 
-        # exceptions at the runtime 
-        try: 
-            
-            # use the microphone as source for input. 
-            with sr.Microphone() as source2: 
-                
-                # wait for a second to let the recognizer 
-                # adjust the energy threshold based on 
-                # the surrounding noise level  
-                r.adjust_for_ambient_noise(source2, duration=0.2) 
-                
-                #listens for the user's input  
-                audio2 = r.listen(source2) 
-                
-                # Using ggogle to recognize audio 
-                MyText = r.recognize_google(audio2) 
-                MyText = MyText.lower() 
     
-                print("Did you say "+MyText)
-                # Splits text input into a list of each word 
-                TextList = MyText.split()
-                return TextList
-                flag = False
-                
-                
-                
-        except sr.RequestError as e: 
-            print("Could not request results; {0}".format(e)) 
-            
-        except sr.UnknownValueError: 
-            print("unknown error occured") 
+    # calls microphone function
+    microphone.microphoneFunction()
+
+    # updates mainText1
+    temporary = "Audio Sent to Server"
+    mainText1.configure(text = temporary)
 
 
 # creates text button
