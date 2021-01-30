e7bc912b4a0baa2b832bd1e7864e208ebd5df98f
William Francis <wmf006@latech.edu>
2021-01-30 15:37:14 -0600

    Update
    Displays when the text/audio is sent to the server with new label.
    
    Does not send text if textbox is empty

--- Code/mainGUI.py
+++ Code/mainGUI.py
@@ -5,35 +5,47 @@ from tkinter import *
 # sets up the main window
 main = Tk()
 main.title("Object Finder Application")
-main.geometry('435x300')
+main.geometry('424x300')
 
-# creates mainText1
-mainText1 = Label(main, text = "            Please Select Form of Input")
-mainText1.grid(column=2, row=1)
+# creates mainText label
+mainText = Label(main, text = "Use the Text/Audio Options Below")
+mainText.grid(column=2, row=1)
+
+# creates mainText0 label
+mainText0 = Label(main, text = "to Search for your Object.")
+mainText0.grid(column=2, row=2)
 
 # creates text entry bot
 textInput1 = Entry(main, width=10)
 textInput1.grid(column=1, row=5) 
 
+# creates mainText1 label
+mainText1 = Label(main, text = " ")
+mainText1.grid(column=2, row=7)
 
 
 def textFunctionCall():
-    # updates mainText1
-    temporary = "              Text Function Called"
-    mainText1.configure(text = temporary)
+    # checks if textbox is empty
+    if textInput1.get() == "" :
+        temporary = "Error: Text Field is Empty"
+        mainText1.configure(text = temporary)
+    else :
+        # calls text function
+        text.textFunction(textInput1.get())
 
-    # calls text function
-    text.textFunction(textInput1.get())
+        # updates mainText1
+        temporary = "Text Sent to Server"
+        mainText1.configure(text = temporary)
 
 
 def microphoneFunctionCall():
-    # updates mainText1
-    temporary = "Microphone Function Called"
-    mainText1.configure(text = temporary)
- 
+    
     # calls microphone function
     microphone.microphoneFunction()
 
+    # updates mainText1
+    temporary = "Audio Sent to Server"
+    mainText1.configure(text = temporary)
 
 
 # creates text button
@@ -43,7 +55,6 @@ textButton.grid(column=1, row=6)
 # creates microphone button
 microphoneButton = Button(main, text = "Search with Microphone", fg = "red", command=microphoneFunctionCall)
 microphoneButton.grid(column=3, row=6)
-
 
 
 # executes main window
