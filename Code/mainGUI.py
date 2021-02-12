acd3bf1502e0a7bc0065e62bfd7519ec1a90dbdb
Devon Knudsen <kingdevon12@gmail.com>
2021-02-12 00:46:15 -0600

    added TCP network capabilities to server and GUI. server now formats queried information and sends back to the GUI. GUI sends requests, receives info from server and disaplays it to user

--- Code/mainGUI.py
+++ Code/mainGUI.py
@@ -1,61 +1,47 @@
 import text
 import microphone
 from tkinter import *
-
 import speech_recognition as sr 
 import pyttsx3
 from socket import *
+from base64 import b64decode
+from io import BytesIO
 
 SERVER_DOMAIN = "192.168.1.54"
 SERVER_PORT = 12001
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+    
+def receiveResponse():
+    bytesReceived = bytearray()
+    dataChunk = clientSocket.recv(BUFFER_SIZE)
+    while (dataChunk != None):
+        bytesReceived.append(dataChunk)
+        dataChunk = clientSocket.recv(BUFFER_SIZE)
+    
+    # decode and seperate the information
+    unencodedResponse = bytesReceived.decode()
+    dateTime, roomID, encryptedImageData = unencodedResponse.split(SEPARATOR)
+    unencryptedImageData = b64decode(encryptedImageData)
+    
+    return dateTime, roomID, unencryptedImageData
 
-def sendRequest(requestedItem):
+# to possibly replace the last else in text/microphone functions
+def findObject(requestedItem):
+    # send requested object to the server
     clientSocket.sendall(requestedItem.encode())
+            
+    # receive response from server
+    dateTime, roomID, unencryptedImageData = receiveResponse()
     
-# connect to server socket
-clientSocket = create_connection((SERVER_DOMAIN, SERVER_PORT))
-sendRequest("PHONE")
-exit(0)
-
-# sets up the main window
-main = Tk()
-main.title("Object Finder Application")
-main.geometry('480x700')
-main.configure(background='lightgrey')
-
-# creates the image area for database pic (NOT WORKING: I just have two test images at the moment)
-display = Canvas(main, width = 300, height = 300)      
-display.place(x =80, y = 300)   
-defaultImage = PhotoImage(file=r"Code\default.png")  
-databaseImage = PhotoImage(file=r"Code\wallet.png")    
-displayVar = display.create_image(20,20, anchor=NW, image=defaultImage)
-
-# creates mainText label
-mainText = Label(main, text = "Use the Text/Audio Options Below", padx = 5, font = ("Nunito Sans", 9, "bold"))
-mainText.place(x = 135, y = 20)
-mainText.configure(background='lightgrey')
-
-# creates mainText0 label
-mainText0 = Label(main, text = "to Search for your Object.", padx = 5, font = ("Nunito Sans", 9, "bold"), width=27)
-mainText0.place(x = 135, y = 40)
-mainText0.configure(background='lightgrey')
-
-# creates text entry bot
-textInput1 = Entry(main, width=15)
-textInput1.place(x = 85, y = 100) 
-
-# hidden text under buttons
-mainText1 = Label(main, text = "", padx = 5, font = ("Nunito Sans", 9, "bold"))
-mainText1.place(x = 170, y = 200)
-mainText1.configure(background='lightgrey')
-
-# hidden text under picture
-mainText2 = Label(main, text = "", padx = 5, font = ("Nunito Sans", 9, "bold"))
-mainText2.place(x = 80, y = 615)
-mainText2.configure(background='lightgrey')
-
-# def receiveResponse():
+    # display information
+    imageMemoryStream = BytesIO(unencryptedImageData)
+    display.itemconfig(displayVar, image = imageMemoryStream)
+    mainText2.configure(text = ("I have found your " + requestedItem + " in the " + roomID + " at " + dateTime))
     
+    # clear in-memory byte stream
+    imageMemoryStream.seek(0)
+    imageMemoryStream.truncate(0)
 
 def textFunctionCall():
     # calls text function
@@ -95,7 +81,45 @@ def microphoneFunctionCall():
         display.itemconfig(displayVar, image=databaseImage)
         mainText2.configure(text = ("I have found your " + objectF + " in the " + roomID + " at " + timeStamp))
 
+# connect to server socket
+clientSocket = create_connection((SERVER_DOMAIN, SERVER_PORT))
 
+# sets up the main window
+main = Tk()
+main.title("Object Finder Application")
+main.geometry('480x700')
+main.configure(background='lightgrey')
+
+# creates the image area for database pic (NOT WORKING: I just have two test images at the moment)
+display = Canvas(main, width = 300, height = 300)      
+display.place(x =80, y = 300)   
+defaultImage = PhotoImage(file=r"Code\images\default.png")  
+databaseImage = PhotoImage(file=r"Code\images\wallet.png")    
+displayVar = display.create_image(20,20, anchor=NW, image=defaultImage)
+
+# creates mainText label
+mainText = Label(main, text = "Use the Text/Audio Options Below", padx = 5, font = ("Nunito Sans", 9, "bold"))
+mainText.place(x = 135, y = 20)
+mainText.configure(background='lightgrey')
+
+# creates mainText0 label
+mainText0 = Label(main, text = "to Search for your Object.", padx = 5, font = ("Nunito Sans", 9, "bold"), width=27)
+mainText0.place(x = 135, y = 40)
+mainText0.configure(background='lightgrey')
+
+# creates text entry bot
+textInput1 = Entry(main, width=15)
+textInput1.place(x = 85, y = 100) 
+
+# hidden text under buttons
+mainText1 = Label(main, text = "", padx = 5, font = ("Nunito Sans", 9, "bold"))
+mainText1.place(x = 170, y = 200)
+mainText1.configure(background='lightgrey')
+
+# hidden text under picture
+mainText2 = Label(main, text = "", padx = 5, font = ("Nunito Sans", 9, "bold"))
+mainText2.place(x = 80, y = 615)
+mainText2.configure(background='lightgrey')
         
 # creates text button
 textButton = Button(main, text = "Search with Text", fg = "blue", bg = "white", command=textFunctionCall, padx = 5, pady = 5, font = ("Sans Serif", 10))
--- Code/server.py
+++ Code/server.py
@@ -1,102 +1,67 @@
 from pymongo import MongoClient
-from base64 import b64decode
-from PIL import Image
-from io import BytesIO
 from socket import *
+from datetime import datetime
 
 # host machine ip, mongodb and server listening port
 DATABASE_DOMAIN = SERVER_DOMAIN = "192.168.1.54"
 DATABASE_PORT = 27017
 SERVER_PORT = 12001
+SEPARATOR = "<SEPARATOR>"
 
-
-def queryDatabase(object):
+def queryDatabase(object, collection):
     # retrieve the newest document from the collection based on datetime (will be changed later to include CNN results)
-    for entry in camNodeResultsCollection.find().sort("dateTime", -1):
+    for entry in collection.find().sort("dateTime", -1):
         newestEntry = entry
         break
     
-    # retrieve the image data and date time
-    encryptedImageData = newestEntry["image"]
-    dateAndTime = newestEntry["dateTime"]
-
-    # unencrypt b64 encrypted image data
-    unencryptedImageData = b64decode(encryptedImageData)
-
-    # CAN BE USED IN THE GUI
-    # # create in-stream memory for image data
-    # imageMemoryStream = BytesIO(unencryptedImageData)
-    # # temporarily display queried image
-    # image = Image.open(BytesIO(unencryptedImageData))
-    # image.show()
-    # # clear in-memory byte stream
-    # imageMemoryStream.seek(0)
-    # imageMemoryStream.truncate(0)
-
-    # CURRENT TEMPORARY WAY TO SAVE QUERIED IMAGES
-    # save unencrypted image to a folder known as serverImgs
-    with open('../serverImgs/{}.jpeg'.format(dateAndTime), mode='wb') as newImage:
-        unencryptedImageData = bytearray(unencryptedImageData)
-        newImage.write(unencryptedImageData)
-        print("[+] Entry image saved in serverImgs folder with name: {}.jpeg".format(dateAndTime))
-        newImage.close()
-
-def establishDatabaseConnection():
-    # connect to the database
-    try:
-        print("Connecting to mongoDB server @ {}:{}...".format(DATABASE_DOMAIN, DATABASE_PORT))
-        mongo_client = MongoClient("mongodb://{}:{}".format(DATABASE_DOMAIN, DATABASE_PORT))
-        print("Connected!")
-    except:
-        print("Connection failed")
-        exit(0)
-
-    # authenticate to database
-    try:
-        rPiDatabase = mongo_client.rPiData
-        print("Authenticating to rPiData database...")
-        rPiDatabase.authenticate(name='serverNode', password='7$dsV!G3D0Oc')
-        print("Sucessfully Authenticated!")
-    except:
-        print("Authentication failed")
-        exit(0)
-
-    # switch to correct collection
-    try:
-        print("Switching to camNodeResults collection...")
-        camNodeResultsCollection = rPiDatabase.camNodeResults
-        print("Successfully switched!")
-    except:
-        print("Switch failed!")
+    # extract and format necessary information
+    necessaryInfo = str(newestEntry["dateTime"] + SEPARATOR + newestEntry["roomID"] + newestEntry["image"])
     
-# def setupServer():
-    
-#     # create TCP server socket and bind to PORT
-#     serverSocket = socket(AF_INET, SOCK_STREAM)
-#     serverSocket.bind((SERVER_DOMAIN, SERVER_PORT))
-    
-#     # allow socket to listen for connections
-#     serverSocket.listen()
-#     print("Server is ready to receive connections!")
-    
-#     return serverSocket
-    
-    
-# MAIN
+    return necessaryInfo
 
-# establishDatabaseConnection()
+# connect to the database
+try:
+    print("Connecting to mongoDB server @ {}:{}...".format(DATABASE_DOMAIN, DATABASE_PORT))
+    mongo_client = MongoClient("mongodb://{}:{}".format(DATABASE_DOMAIN, DATABASE_PORT))
+    print("Connected!")
+except:
+    print("Connection failed")
+    exit(0)
+
+# authenticate to database
+try:
+    rPiDatabase = mongo_client.rPiData
+    print("Authenticating to rPiData database...")
+    rPiDatabase.authenticate(name='serverNode', password='7$dsV!G3D0Oc')
+    print("Sucessfully Authenticated!")
+except:
+    print("Authentication failed")
+    exit(0)
+
+# switch to correct collection
+try:
+    print("Switching to camNodeResults collection...")
+    camNodeResultsCollection = rPiDatabase.camNodeResults
+    print("Successfully switched!")
+except:
+    print("Switch failed!")
 
 # create TCP server socket and bind to PORT
 serverSocket = create_server((SERVER_DOMAIN, SERVER_PORT))
-print("Server is ready to receive connections!")
+
+# keep socket listening for a single connection
+serverSocket.listen(1)
+print("Server is ready to receive a connection...")
 
 while True:
-    # create connection socket and receive request message from connection socket
+    # create connection socket
     connectionSocket, address = serverSocket.accept()
     print("Connection from {} accepted!".format(address))
-    incomingRequest = connectionSocket.recv(1024).decode()
-    print("Item requested from client: {}".format(incomingRequest))
-
     
-# for testing purposes only
-# queryDatabase("phone")
+    # receive request message from connection socket
+    incomingRequest = connectionSocket.recv(1024).decode()
+    print("Item requested from client: {} at {}".format(incomingRequest, datetime.now()))
+    
+    # query database and send requested information from query to sender
+    requestedInformation = queryDatabase(incomingRequest, camNodeResultsCollection)
+    serverSocket.sendall(requestedInformation.encode())
