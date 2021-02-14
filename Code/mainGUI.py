f180cdd31448a4918a5fadeefecaadc36e3d9fc8
Devon Knudsen <kingdevon12@gmail.com>
2021-02-13 18:01:57 -0600

    print len of datachunk

--- Code/mainGUI.py
+++ Code/mainGUI.py
@@ -21,7 +21,7 @@ def receiveResponse():
     while True:
         dataChunk = clientSocket.recv(BUFFER_SIZE)
         bytesReceived.extend(dataChunk)
-        print(dataChunk)
+        print(len(dataChunk))
         if(len(dataChunk) < 1):
             break
     
