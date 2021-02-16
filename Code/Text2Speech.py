062095ed32471ca8644e58e1cf26a8cf21d9df25
William Francis <wmf006@latech.edu>
2021-02-16 15:52:57 -0600

    Working Text 2 Speech

--- Code/Text2Speech.py
+++ Code/Text2Speech.py
@@ -1,6 +1,23 @@
 import pyttsx3
 
+requestedItem = "wallet"
+roomID = "3"
+dateTime = "December 23, 2021"
+clockTime = "4:00pm"
 
+#object creations
 speech = pyttsx3.init()
-speech.say("TEST sdjkfhsdf TEST")
+
+#sets speaking rate
+speech.setProperty('rate', 130)
+
+#sets volume
+speech.setProperty('volume',1.0)
+
+#sets voice to female
+voices = speech.getProperty('voices')
+speech.setProperty('voice', voices[1].id)
+
+#output speech
+speech.say("I have found your " + requestedItem + " in room " + roomID + " on " + dateTime + " at " + clockTime)
 speech.runAndWait()
