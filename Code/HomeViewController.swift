1b2302544215f4ad551ef3821bbfd5739a8dc135
Branson Hanzo <37875211+brans0nH@users.noreply.github.com>
2021-03-11 16:56:48 -0600

    View controllers

---
+++ Code/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  ObjectFinder
+//
+//  Created by Branson Hanzo on 2/21/21.
+//
+
+import UIKit
+// import SideMenu
+
+class HomeViewController:
+    UIViewController {
+    
+    // var menu: SideMenuNavigationController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
