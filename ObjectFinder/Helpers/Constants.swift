4b3ad9a3314e546a514e883e136006068402e6e6
Branson Hanzo <37875211+brans0nH@users.noreply.github.com>
2021-03-11 17:05:24 -0600

    Object Finder app code

---
+++ ObjectFinder/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ObjectFinder
+//
+//  Created by Branson Hanzo on 2/22/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+}
