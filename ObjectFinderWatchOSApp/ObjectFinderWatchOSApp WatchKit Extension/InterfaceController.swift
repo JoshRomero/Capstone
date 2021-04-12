39e47a1dab39465c490c2b0e3902712e58c382e3
William Francis <williamfrancis006@gmail.com>
2021-04-12 16:32:19 -0500

    Updates to WatchOS

--- ObjectFinderWatchOSApp/ObjectFinderWatchOSApp WatchKit Extension/InterfaceController.swift
+++ ObjectFinderWatchOSApp/ObjectFinderWatchOSApp WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,9 @@ var loggedin = false
 
 class InterfaceController: WKInterfaceController {
     
+    
+    @IBOutlet var SearchButton: WKInterfaceButton!
+    
     @IBOutlet var StartButton: WKInterfaceButton!
     
     @IBOutlet var TextLabel: WKInterfaceLabel!
@@ -19,8 +22,6 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var TextLabel2: WKInterfaceLabel!
     
     @IBOutlet private var loadingLabel: WKInterfaceLabel!
-    private var loadingTimer = Timer()
-    private var progressTracker = 1
     
     
     override func awake(withContext context: Any?) {
@@ -139,47 +140,102 @@ class InterfaceController: WKInterfaceController {
         }
     }
     
-    @IBAction func ResetButtonPressed() {
+    
+    
+    @IBAction func HomeButtonPressed() {
+        loggedin = false
         self.pushController(withName: "Start Screen", context: nil)
-
     }
+    
         
     @IBAction func SearchButtonPressed() {
-        let options = ["Keys", "Wallet"]
-        self.presentTextInputController(withSuggestions: options, allowedInputMode: .plain, completion: { results in
-            guard let results = results else { return }
-            OperationQueue.main.addOperation {
-                self.dismissTextInputController()
-                let UserInput = (results[0] as? String)
-                let LowercaseInput = UserInput?.lowercased()
-                
-                if LowercaseInput!.contains("key") {
-                    self.TextLabel.setText("Keys")
-                    self.TextLabel2.setText("Searching for:")
+        loggedin = false
+        
+        // grab the information from the app and update the UI in the background
+        DispatchQueue.global(qos: .userInitiated).async {
+            DispatchQueue.global(qos: .userInitiated).async {
+                var count = 0
+                while true{
+                    if count == 1{
+                        // updating the UI
+                        DispatchQueue.main.async {
+                            self.SearchButton.setTitle("Loading.")
+                        }
+                    }else if count == 2 {
+                        // updating the UI
+                        DispatchQueue.main.async {
+                            self.SearchButton.setTitle("Loading..")
+                        }
+                    }else if count == 3 {
+                        // updating the UI
+                        DispatchQueue.main.async {
+                            self.SearchButton.setTitle("Loading...")
+                        }
+                    }else{
+                        // updating the UI
+                        DispatchQueue.main.async {
+                            self.SearchButton.setTitle("Loading")
+                        }
+                    }
                     
+                    count += 1
+                    sleep(1)
+                    if count == 4 && !loggedin{
+                        count = 0
+                    }
+                    else if loggedin{
+                        sleep(1)
+                        self.SearchButton.setTitle("Search Again")
+                        break
+                    }
                 }
-                
-                else if LowercaseInput!.contains("keys") {
-                    self.TextLabel.setText("Keys")
-                    self.TextLabel2.setText("Searching for:")
-                }
-                
-                else if LowercaseInput!.contains("wallet") {
-                    self.TextLabel.setText("Wallet")
-                    self.TextLabel2.setText("Searching for:")
-                }
-                
-                else if LowercaseInput!.contains("wallets") {
-                    self.TextLabel.setText("Wallet")
-                    self.TextLabel2.setText("Searching for:")
-                }
-                
-                else {
-                    self.TextLabel.setText("Error, Please Try Again")
-                }
-                
             }
-            })
+            
+            
+            // get data from app
+            let returned = self.check_if_logged_in()
+            // after getting the logged in results update the UI
+            if returned && loggedin{
+                let options = ["Keys", "Wallet"]
+                self.presentTextInputController(withSuggestions: options, allowedInputMode: .plain, completion: { results in
+                    guard let results = results else { return }
+                    OperationQueue.main.addOperation {
+                        self.dismissTextInputController()
+                        let UserInput = (results[0] as? String)
+                        let LowercaseInput = UserInput?.lowercased()
+                        
+                        if LowercaseInput!.contains("key") {
+                            self.TextLabel.setText("Keys")
+                            self.TextLabel2.setText("Searching for:")
+                            
+                        }
+                        
+                        else if LowercaseInput!.contains("keys") {
+                            self.TextLabel.setText("Keys")
+                            self.TextLabel2.setText("Searching for:")
+                        }
+                        
+                        else if LowercaseInput!.contains("wallet") {
+                            self.TextLabel.setText("Wallet")
+                            self.TextLabel2.setText("Searching for:")
+                        }
+                        
+                        else if LowercaseInput!.contains("wallets") {
+                            self.TextLabel.setText("Wallet")
+                            self.TextLabel2.setText("Searching for:")
+                        }
+                        
+                        else {
+                            self.TextLabel.setText("Error, Please Try Again")
+                        }
+                    }
+                    })
+            }else{
+                DispatchQueue.main.async {
+                    self.pushController(withName: "Login", context: nil)
+                }
+            }
+        }
     }
 }
 
