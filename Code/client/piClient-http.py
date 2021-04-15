e90b200be7241120b6dba3229d1318a446cf5dd9
Devon Knudsen <kingdevon12@gmail.com>
2021-04-15 13:21:05 -0500

    most updated version of the pi http client

--- Code/client/piClient-http.py
+++ Code/client/piClient-http.py
@@ -5,6 +5,31 @@ from io import BytesIO
 import ssl
 import requests
 import tempfile
+from base64 import b64encode
+import json
+import pyrebase
+
+email = "johndoe@gmail.com"
+password = "Pa55w0rd!"
+
+# NEED TO: RESOLVE UNICODE ISSUE WITH IMAGE BYTES -- CURRENTLY CANNOT SEND IMAGES IN IN ENTRY FOR SOME REASON
+# CHECK PI FOR NEWEST CODE
+
+config = {
+  "apiKey": "AIzaSyDkYMP_ilWmPr5n0Kt_N7odVehYEw6qh64",
+  "authDomain": "objectfinder-3d3f3.firebaseapp.com",
+  "databaseURL": "https://objectfinder-3d3f3-default-rtdb.firebaseio.com",
+  "storageBucket": "objectfinder-3d3f3.appspot.com"
+}
+firebase = pyrebase.initialize_app(config)
+
+# Get a reference to the auth service
+auth = firebase.auth()
+
+# Log the user in
+user = auth.sign_in_with_email_and_password(email, password)
+print(user)
+header = {"Authorization": user["idToken"]}
 
 ROOM_ID = 1
 items = {"keysProb": 0.0, "glassesProb": 0.0, "remoteProb": 0.0}
@@ -33,6 +58,7 @@ if __name__ == "__main__":
             sleep(.5)
             
             tmpImage = tempfile.NamedTemporaryFile(suffix = ".jpeg")
+            print(tmpImage.name)
             
             # capture image and append to in-memory byte stream
             captureTime = datetime.now()
@@ -40,16 +66,17 @@ if __name__ == "__main__":
             print("[+] Picture captured at the dateTime: {}".format(captureTime))
                 
             # create database entry
-            uid = "testID" # temporary, needs to be replaced by token
+            #uid = "qNZFhdvgnIgdSnORNpmbgqlFO7S2" # temporary, needs to be replaced by token
             items["keysProb"] = 1.0  # temporary, needs to be replaced with tensorflow data
             imageData = tmpImage.read()
-            
+            entry = createEntry(user["localId"], captureTime)
+
             file = {"image": open(tmpImage.name, 'rb')}
-            entry = createEntry(uid, captureTime)
             
             # send post request to server to insert image and related data
-            url = "http://objectfinder.tech/pidata"
-            r = requests.post(url, files=file, data=entry)
+            url = "https://objectfinder.tech/pidata"
+            r = requests.post(url, files=file, data=entry, headers=header)
+            print(r)
             
             # reset probability values for each item
             for item in items:
@@ -57,4 +84,3 @@ if __name__ == "__main__":
                 
             # sleep a minute to give the database time to receive the last entry
             sleep(10)
-
