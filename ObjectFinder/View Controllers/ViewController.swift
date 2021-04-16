a439fa8f46a1e65f55f01a810f73a1d76493e61e
JoshRomero <38444415+JoshRomero@users.noreply.github.com>
2021-04-15 19:38:57 -0500

    fixed a bug

--- ObjectFinder/View Controllers/ViewController.swift
+++ ObjectFinder/View Controllers/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var got_message = false
     var message = ""
     var session: WCSession?
+    var logged_in = true
     
     @IBOutlet weak var signUpButton: UIButton!
     
@@ -28,6 +29,13 @@ class ViewController: UIViewController {
         }
     }
     
+    func get_from_DB(data: String) -> String
+    {
+        // search in DB for the proper results
+        return "results"
+    }
+    
+    func BoolToString(b: Bool?)->String { return b?.description ?? "<None>"}
     
     func listening()
     {
@@ -36,15 +44,27 @@ class ViewController: UIViewController {
             {
                 if self.got_message
                 {
+                    if self.message == "logged in?"
+                    {
+                        let return_message = self.BoolToString(b: self.logged_in)
+                        if let validSession = self.session, validSession.isReachable {
+                        validSession.sendMessage(["iPhone": return_message], replyHandler: nil, errorHandler: nil)
+                          }
+                    }else{
+                        
+                        let return_message = self.get_from_DB(data: self.message)
+                        if let validSession = self.session, validSession.isReachable {
+                        validSession.sendMessage(["iPhone": return_message], replyHandler: nil, errorHandler: nil)
+                          }
+                    }
                     self.got_message = false
-                    if let validSession = self.session, validSession.isReachable {
-                    validSession.sendMessage(["iPhone": "true"], replyHandler: nil, errorHandler: nil)
-                      }
+                    
                 }
             }
         }
         
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,9 +88,9 @@ class ViewController: UIViewController {
     
     @IBAction func test_tap(_ sender: Any) {
         
-        if let validSession = self.session, validSession.isReachable {
-        validSession.sendMessage(["iPhone": "clock"], replyHandler: nil, errorHandler: nil)
-          }
+//        if let validSession = self.session, validSession.isReachable {
+//        validSession.sendMessage(["iPhone": "clock"], replyHandler: nil, errorHandler: nil)
+//          }
     }
     
     
@@ -80,10 +100,10 @@ extension ViewController: WCSessionDelegate {
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
  }
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        self.got_message = true
          if let value = message["watch"] as? String {
             self.message = value
          }
+        self.got_message = true
         
         print("Iphone got message from watch:",self.message)
        }
