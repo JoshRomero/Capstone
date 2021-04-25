8980936b35324bd94bf3771476e451da3118ca50
JoshRomero <38444415+JoshRomero@users.noreply.github.com>
2021-04-25 10:00:30 -0500

    Resolved merge conflict by incorporating both suggestions.

--- Code/server/dashboard.py
+++ Code/server/dashboard.py
@@ -1,63 +1,30 @@
 # This script count the number of times each object has been queried by the user
 
-'''
-
-THIS BLOCK OF CODE WAS WHEN WE WERE CONNECTING TO DB FROM DASHBOARD CODE
-
-from pymongo import MongoClient
-# IP and port of EC2 instance
-DATABASE_DOMAIN = "18.188.84.183"
-DATABASE_PORT = 27017
-
-# connect and authenticate and switch to correct collection
-try:
-    print("Connecting to mongoDB server @ {}:{}...".format(DATABASE_DOMAIN, DATABASE_PORT))
-    mongo_client = MongoClient("mongodb://{}:{}".format(DATABASE_DOMAIN, DATABASE_PORT))
-    print("Connected!")
-except:
-    print("Connection failed")
-    exit(0)
-try:
-    rPiDatabase = mongo_client.rPiData
-    print("Authenticating to rPiData database...")
-    rPiDatabase.authenticate(name='serverNode', password='7$dsV!G3D0Oc')
-    print("Sucessfully Authenticated!")
-except:
-    print("Authentication failed")
-    exit(0)
-try:
-    print("Switching to camNodeResults collection...")
-    camNodeResultsCollection = rPiDatabase.camNodeResults
-    print("Successfully switched!")
-except:
-    print("Switch failed!")
-'''
-
 
 import matplotlib.pyplot as plt
 
 def dashboard(collection):
     ######## START GET DATA FOR OBJECT QUERIES BY TYPE ############################################################
     # initialize all counts to 0
-    backPackQueries = 0
-    suitcaseQueries = 0
+    remoteQueries = 0
+    handbagQueries = 0
     laptopQueries = 0
     cellPhoneQueries = 0
-    umbrellaQueries = 0
+    bookQueries = 0
     
-    # search for and count all backpack queries
-    curObject = "backpack"
+    # search for and count all remote queries
+    curObject = "remote"
     listQueries = collection.find({ "objectQueried": curObject}, { "objectQueried": 1 } )
     
     for x in listQueries:
-      backPackQueries += 1
+      remotekQueries += 1
     
-    # search for and count all suitcase queries
-    curObject = "suitcase"
+    # search for and count all handbag queries
+    curObject = "handbag"
     listQueries = collection.find({ "objectQueried": curObject}, { "objectQueried": 1 } )
     
     for x in listQueries:
-      suitcaseQueries += 1
+      handbagQueries += 1
     
     # search for and count all laptop queries
     curObject = "laptop"
@@ -73,12 +40,12 @@ def dashboard(collection):
     for x in listQueries:
       cellPhoneQueries += 1
     
-    # search for and count all umrella queries
-    curObject = "umbrella"
+    # search for and count all book queries
+    curObject = "book"
     listQueries = collection.find({ "objectQueried": curObject}, { "objectQueried": 1 } )
     
     for x in listQueries:
-      umbrellaQueries += 1
+      bookQueries += 1
     ######## END GET DATA FOR OBJECT QUERIES BY TYPE ############################################################
     
     
@@ -106,8 +73,8 @@ def dashboard(collection):
     # first subplot is how many queries by item type
     plt.title("Your Dashboard")
     plt.subplot(2,1,1)
-    objNames = ["backpack", "suitcase", "laptop", "cellphone", "umbrella"]
-    numQueries = [backPackQueries, suitcaseQueries, laptopQueries, cellPhoneQueries, umbrellaQueries]
+    objNames = ["remote", "handbag", "laptop", "cellphone", "book"]
+    numQueries = [remoteQueries, handbagQueries, laptopQueries, cellPhoneQueries, bookQueries]
     plt.bar(objNames, numQueries)
     
     # second subplot is a lines plot how many queries by time of day
